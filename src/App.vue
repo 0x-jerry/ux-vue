1bbec445e7a0630c029c314568bde945e5ecff3d
Gem Xli <x.li.gem@gmail.com>
2021-03-08 18:45:24 +0800

    feat: update app style

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <h1>Use vue to explore how to implement a better UX interface</h1>
-  <Navigation></Navigation>
+  <div class="box">
+    <h1 class="title">Use vue to explore how to implement a better UX interface</h1>
+  </div>
+  <div class="box">
+    <h2 class="title">1. Navigation component with amazing animation</h2>
+    <Navigation></Navigation>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import Navigation from './components/Navigation.vue'
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #393d41;
+}
+
+.box {
+  color: #adadad;
+
+  &:hover {
+    color: #333333;
+  }
+
+  .title {
+    transition: color ease 0.4s;
+  }
 }
 
 button {
